bfca3c0e19e94e0e52e0e2b90539dcb093b72038
Facumerino03 <fa.merino@alumno.um.edu.ar>
2025-05-28 16:40:27 -0300

    Initialize spring boot project

---
+++ src/test/java/com/example/finquik/FinquikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finquik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinquikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
